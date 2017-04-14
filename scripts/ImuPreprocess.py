8768838815bef0d1767289bf4fc8d88163a0443a
steve <551619855@qq.com>
2017-04-14 15:27:24 +0800

    zero-velocity detector error...

--- scripts/ImuPreprocess.py
+++ scripts/ImuPreprocess.py
@@ -7,8 +7,8 @@ import scipy as sp
 
 import matplotlib.pyplot as plt
 
-from OPENSHOE import zupt_test
-from OPENSHOE import Setting
+# from OPENSHOE import zupt_test
+from scripts.OPENSHOE import Setting, zupt_test
 
 
 class ImuPreprocess:
@@ -18,22 +18,34 @@ class ImuPreprocess:
         :param source_data_file: 
         '''
         self.data = np.loadtxt(source_data_file, dtype=float, delimiter=',')
-        self.para = Setting.setting()
+        # use rad replace deg
+        self.data[:, 4:7] = self.data[:, 4:7] * np.pi / 180.0
+        self.data[:, 1:4] = self.data[:, 1:4] * 9.8
+
+        self.para = Setting.settings()
+        # self.para.time_Window_size = 40
 
     def computezupt(self):
         '''
         
         :return: 
         '''
-        zupt_detector = zupte_test()
+        zupt_detector = zupt_test.zupte_test(self.para)
         self.zupt_result = zupt_detector.GLRT_Detector(self.data[:, 1:7])
 
         # debug
 
-        plt.figure(1)
+        plt.figure()
+        plt.title("ZUPT detector")
         plt.grid(True)
+        for i in range(1, 4):
+            plt.plot(self.data[:, i])
         plt.plot(self.zupt_result)
         plt.show()
+
+    def ShowMagnety(self):
+        plt.figure()
+
 
 
 if __name__ == '__main__':
