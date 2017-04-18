5f40cf8544179c9f75ae948f609ee0fef4e012d7
steve <551619855@qq.com>
2017-04-18 19:20:36 +0800

    try to direct read quant and pose from file save by python scripts.

--- src/graph_build_without_ekf.cpp
+++ src/graph_build_without_ekf.cpp
@@ -1,4 +1,199 @@
 //
 // Created by steve on 17-4-18.
 //
+#include <iostream>
+#include <thread>
 
+#include <fstream>
+
+
+//#include "Eigen/Dense"
+//#include "Eigen/Geometry"
+#include<Eigen/Dense>
+#include <Eigen/Geometry>
+
+#include "Extent/CSVReader.h"
+#include "ZUPT/MYEKF.h"
+#include "ZUPT/EKF.hpp"
+
+#include "Extent/time_stamp.h"
+#include "Extent/matplotlib_interface.h"
+
+#include "g2o/core/sparse_optimizer.h"
+#include "g2o/core/block_solver.h"
+#include "g2o/core/factory.h"
+#include "g2o/core/optimization_algorithm_levenberg.h"
+#include "g2o/solvers/csparse/linear_solver_csparse.h"
+#include "g2o/types/slam3d/types_slam3d.h"
+#include "g2o/types/slam3d_addons/types_slam3d_addons.h"
+
+#include "g2o/core/robust_kernel.h"
+#include "g2o/core/robust_kernel_factory.h"
+
+#include "OwnEdge/ZoEdge.h"
+#include "OwnEdge/ZoEdge.cpp"
+#include "OwnEdge/DistanceEdge.h"
+#include "OwnEdge/DistanceEdge.cpp"
+
+G2O_USE_TYPE_GROUP(slam3d)
+
+
+int main(int argc, char *argv[]) {
+
+    /// Load Data
+    CSVReader vertex_pose_file("./TMP_DATA/vertex_pose.csv");
+    CSVReader vertex_quat_file("./TMP_DATA/vertex_quat.csv");
+    CSVReader close_id_file("./TMP_DATA/close_vetices_num.csv");
+
+    auto vertex_pose(vertex_pose_file.GetMatrix());
+    auto vertex_quat(vertex_quat_file.GetMatrix());
+    auto close_id(close_id_file.GetMatrix());
+
+
+    /// Initial graph
+    g2o::SparseOptimizer globalOptimizer;
+
+
+    typedef g2o::BlockSolver_6_3 SlamBlockSolver;
+    typedef g2o::LinearSolverCSparse<SlamBlockSolver::PoseMatrixType> SlamLinearSolver;
+
+    // Initial solver
+    SlamLinearSolver *linearSolver = new SlamLinearSolver();
+    linearSolver->setBlockOrdering(false);
+    SlamBlockSolver *blockSolver = new SlamBlockSolver(linearSolver);
+    g2o::OptimizationAlgorithmLevenberg *solver =
+            new g2o::OptimizationAlgorithmLevenberg(blockSolver);
+    globalOptimizer.setAlgorithm(solver);
+
+
+
+    /// Add distance caonstraint
+    for (int k(0); k < close_id.GetRows(); ++k) {
+        auto edge = new DistanceEdge();
+
+        std::cout << "vertex id : " << int(*close_id(k, 0)) << " to " << int(*close_id(k, 1)) << std::endl;
+        edge->vertices()[0] = globalOptimizer.vertex(int(*close_id(k, 0)));
+        edge->vertices()[1] = globalOptimizer.vertex(int(*close_id(k, 1)));
+        edge->setMeasurement(0.0);
+        Eigen::Matrix<double, 1, 1> information;
+        information(0, 0) = 1.0 / 1.0;
+
+//        if(*close_id(k,0)>337||*close_id(k,1)>337)
+//        {
+//            continue;
+//        }
+
+        edge->setInformation(information);
+
+
+        globalOptimizer.addEdge(edge);
+    }
+
+    /// Build base graph
+
+
+    Eigen::Isometry3d last_t = (Eigen::Isometry3d::Identity());
+
+    for (int index(0); index < vertex_pose.GetRows(); ++index) {
+        /// build transfrom matrix
+
+        Eigen::Isometry3d transform = (Eigen::Isometry3d::Identity());
+
+        Eigen::Quaterniond the_quat;
+        the_quat.x() = *vertex_quat(index, 0);
+        the_quat.y() = *vertex_quat(index, 1);
+        the_quat.z() = *vertex_quat(index, 2);
+        the_quat.w() = *vertex_quat(index, 3);
+
+        Eigen::Matrix3d rotation_matrix = the_quat.normalized().toRotationMatrix();
+
+
+        Eigen::Vector3d offset(
+                *vertex_pose(index, 0),
+                *vertex_pose(index, 1),
+                *vertex_pose(index, 2)
+        );
+
+        for (int ix(0); ix < 3; ++ix) {
+            for (int iy(0); iy < 3; ++iy) {
+                transform(ix, iy) = rotation_matrix(ix, iy);
+            }
+        }
+
+
+
+        /// add vertex
+
+        auto *vertex = new g2o::VertexSE3();
+        vertex->setId(index);
+//        vertex->setEstimate
+        vertex->setEstimate(transform);
+        if (index == 0) {
+            vertex->setFixed(true);
+        }
+        globalOptimizer.addVertex(vertex);
+
+
+        /// add edge
+        if (index > 0) {
+
+            auto *edge = new g2o::EdgeSE3;
+
+            edge->vertices()[0] = globalOptimizer.vertex(index - 1);
+            edge->vertices()[1] = globalOptimizer.vertex(index);
+
+            Eigen::Matrix<double, 6, 6> information = Eigen::Matrix<double, 6, 6>::Identity();
+            information(0, 0) = information(1, 1) = information(2, 2) = 2100;
+            information(3, 3) = information(4, 4) = information(5, 5) = 2000;
+            edge->setInformation(information);
+
+            edge->setInformation(information);
+
+            edge->setMeasurement(last_t.inverse() * transform);
+
+            globalOptimizer.addEdge(edge);
+        }
+
+        last_t = transform;
+
+
+    }
+
+
+
+    /// Optimizer
+    double start_optimize_time = TimeStamp::now();
+//    globalOptimizer.initializeOptimization();
+    globalOptimizer.initializeOptimization();
+    globalOptimizer.optimize(2000);
+    std::cout << " optimize waste time :" << TimeStamp::now() - start_optimize_time << std::endl;
+
+
+    ///Save result
+    std::ofstream trace_file("./TMP_DATA/save_trace.txt");
+
+    std::vector<double> rx, ry, rz;
+    int index = 0;
+    while (true) {
+        auto t_vertex = globalOptimizer.vertex(index);
+        if (t_vertex > 0) {
+            double data[10] = {0};
+            t_vertex->getEstimateData(data);
+
+            rx.push_back(data[0]);
+            ry.push_back(data[1]);
+            rz.push_back(data[2]);
+            trace_file << data[0] << " " << data[1] << " " << data[2] << std::endl;
+            index++;
+        } else {
+            break;
+        }
+//        delete t_vertex;
+    }
+
+    std::cout << "after compute vertex : " << rx.size() << std::endl;
+    trace_file.close();
+
+    matplotlibcpp::plot(rx, ry, "r-*");
+    matplotlibcpp::show();
+}
