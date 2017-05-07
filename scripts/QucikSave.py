27e2ab7cbf7e946e071a40a9438f379dd28478f3
steve <551619855@qq.com>
2017-05-07 22:08:18 +0800

    extract new data.

--- scripts/QucikSave.py
+++ scripts/QucikSave.py
@@ -3,9 +3,15 @@
 
 
 import scripts.ImuResultReader
+import scripts.ImuPreprocess
 
 import numpy as np
 
 if __name__ == '__main__':
     irr = scripts.ImuResultReader.ImuResultReader("/home/steve/Data/10DOFIMU/Record(6).txt")
     np.savetxt("/home/steve/Data/FastUwbDemo/1/sim_imu.csv", irr.data_with_time, "%.18e", delimiter=',')
+
+    ip = scripts.ImuPreprocess.ImuPreprocess("/home/steve/Data/FastUwbDemo/1/sim_imu.csv")
+    ip.computezupt()
+    print(ip.zupt_result)
+    np.savetxt("/home/steve/Data/FastUwbDemo/1/sim_zupt.csv", ip.zupt_result, '%.18e', delimiter=',')
